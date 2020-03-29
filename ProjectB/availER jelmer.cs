f80bb92a438b095e6b3c7199c89f14458f545e79
Jelmer445 <60722473+Jelmer445@users.noreply.github.com>
2020-03-29 20:35:38 +0200

    Add files via upload

---
+++ ProjectB/availER jelmer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace availER
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+
+            string Kamer1 = "beschikbaar";
+            string Kamer2 = "niet beschikbaar";
+            string Kamer3 = "niet beschikbaar";
+            string Kamer4 = "beschikbaar";
+            string Kamer5 = "beschikbaar";
+
+
+            Console.WriteLine("====================================");
+            Console.WriteLine("Overzicht beschikbaarheid van de Escape Rooms." + "\n");
+            Console.WriteLine("Kamer 1: "+ Kamer1 + "\n" + "Kamer 2: " + Kamer2 + "\n" + "Kamer 3: "+ Kamer3 + "\n" + "Kamer 4: " + Kamer4 + "\n" + "Kamer 5: " + Kamer5);
+            Console.WriteLine("====================================");
+            Console.WriteLine("Welke kamer wilt u boeken(1-5)?");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.Clear();
+                    Console.WriteLine("Deze kamer is " + Kamer1 + " voor boeking.");
+                    break;
+                case "2":
+                    Console.Clear();
+                    Console.WriteLine("Deze kamer is " + Kamer2 + " voor boeking.");
+                    break;
+                case "3":
+                    Console.Clear();
+                    Console.WriteLine("Deze kamer is " + Kamer3 + " voor boeking.");
+                    break;
+                case "4":
+                    Console.Clear();
+                    Console.WriteLine("Deze kamer is " + Kamer4 + " voor boeking.");
+                    break;
+                case "5":
+                    Console.Clear();
+                    Console.WriteLine("Deze kamer is " + Kamer5 + " voor boeking.");
+                    break;
+            }
+        }
+    }
+}
