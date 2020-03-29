959f00b7a2fbf0e57fa8414a8febd326b88596c3
Chrizza <Chrizza1@users.noreply.github.com>
2020-03-29 23:34:19 +0200

    EscapeRoom class aangepast eerste versie van de herschreven Crud

--- ProjectB/Crud/EscapeRoom.cs
+++ ProjectB/Crud/EscapeRoom.cs
@@ -7,27 +7,11 @@ namespace ProjectB.Crud
     public class EscapeRoom
     {
 
-        public int roomNumber, ageLimit;
+        public int roomNumber, ageMinimum, roomSize;
         public double roomPrice;
-        public string roomSize, roomTheme, roomDuration, roomReview, roomName;
+        public string roomTheme, roomDuration, roomReview, roomName;
         public bool roomAvailability, isTaken = false;
 
-        public EscapeRoom(int roomNumber) { this.roomNumber = roomNumber; }
-
-        public void setNewValues(
-            int ageLimit,
-            double roomPrice,
-            Boolean roomAvailability,
-            string roomSize, string roomTheme, string roomDuration, string roomName)
-        {
-            this.ageLimit = ageLimit;
-            this.roomPrice = roomPrice;
-            this.roomAvailability = roomAvailability;
-            this.roomSize = roomSize;
-            this.roomTheme = roomTheme;
-            this.roomDuration = roomDuration;
-            this.roomName = roomName;
-        }
 
     }
 }
--- ProjectB/Program.cs
+++ ProjectB/Program.cs
@@ -1,225 +1,71 @@
 ﻿using System;
-using ProjectB.Crud;
 using System.Collections.Generic;
+using ProjectB.Crud;
 
-namespace ProjectB
+class MainProgramma
 {
-    class Program
-    {
-        public static void Main()  
-        {
-            EscapeRoom[] Rooms = 
-                { 
-                new EscapeRoom(1),
-                new EscapeRoom(2),
-                new EscapeRoom(3),
-                new EscapeRoom(4),
-                new EscapeRoom(5) 
-            };
+	public static List<EscapeRoom> RoomsList = new List<EscapeRoom>();
 
-            Console.WriteLine("1: Add room\n2: Edit room\n3:Delete room");
-            string userInput = Console.ReadLine();
-
-            if (userInput == "1") //AddRoom
-            {
-                bool availableRoom = isRoom(Rooms);
-                if (!availableRoom) { Console.WriteLine("No available room spaces, sorry"); }
-                else
-                {
-                    int roomNum = getNextRoom(Rooms);
-                    Rooms[roomNum - 1].isTaken = true;
-
-                    Console.WriteLine("Enter the minimum age (e.g 12) - \n");
-                    string temp = Console.ReadLine();
-                    int ageLimit;
-                    Int32.TryParse(temp, out int eye);
-                    { ageLimit = eye; }
-                    Console.Clear();
-
-                    Console.WriteLine("Enter the room price (e.g 24.95) - \n");
-                    string temp2 = Console.ReadLine();
-                    double roomPrice;
-                    Double.TryParse(temp2, out double i);
-                    { roomPrice = i; }
-                    Console.Clear();
-
-                    Console.WriteLine("Enter the availability (y/n) - \n");
-                    string temp3 = Console.ReadLine();
-                    bool roomAvailability;
-                    bool.TryParse(temp3, out bool y);
-                    { roomAvailability = y; }
-                    Console.Clear();
-
-                    Console.WriteLine("Enter the age roomSize (e.g 2-4) - \n");
-                    string roomSize = Console.ReadLine();
-                    Console.Clear();
-
-                    Console.WriteLine("Enter the room Theme description (string) - \n");
-                    string roomTheme = Console.ReadLine();
-                    Console.Clear();
-
-                    Console.WriteLine("Enter the room duration (e.g 1 hour) - \n");
-                    string roomDuration = Console.ReadLine();
-                    Console.Clear();
-
-                    Console.WriteLine("Enter the room name - \n");
-                    string roomName = Console.ReadLine();
-                    Console.Clear();
+	public static void Main()
+	{
 
 
-                    Rooms[roomNum - 1].setNewValues(ageLimit, roomPrice, roomAvailability, roomSize, roomTheme, roomDuration, roomName);
-                }              
-            }
-            else if(userInput == "2") //Edit
-            {
+		Console.WriteLine("1: Add room\n2: Edit room\n3: Delete room");
+		int InterFaceInput = Convert.ToInt32(Console.ReadLine());
 
-            }
-            static bool isRoom(EscapeRoom[] givenRooms) {
-                for (int i = 0; i < givenRooms.Length; i++) {
-                    if (!(givenRooms[i].isTaken)) { return true; }
-                    
-                }
-                return false;
-            }
+		if (InterFaceInput == 1) { AddFunction(RoomsList); }
+		if (InterFaceInput == 2) { EditFunction(RoomsList); }
+		if (InterFaceInput == 3) { DeleteFunction(RoomsList); }
 
-            static int getNextRoom(EscapeRoom[] givenRooms)
-        {
-            //int availableRoom;
-            for (int i = 0; i < givenRooms.Length; i++) {
-                    if (!(givenRooms[i].isTaken)) { return givenRooms[i].roomNumber; }
-            }
-            return -1;
-        }
-            //escaperoom room1 = new escaperoom();
-            //room1.roomnumber = 1;
-            //room1.roomname = "horror";
-            //room1.agelimit = 18;
-            //room1.roomavailability = false;
-            //room1.roomtheme = "deze escape room bestaat uit een horror thema met enge puzzels en evenementen.";
-            //room1.roomduration = "1 uur";
-            //room1.roomreview = "";
-            //room1.roomprice = 23.95;
-            //room1.roomsize = "3-5 mensen";
+		static void AddFunction(List<EscapeRoom> RoomsList)
+		{
+			if (RoomsList.Count > 0 & RoomsList.Count < 4)
+			{
+				RoomsList.Add(new EscapeRoom() { });
+				int NewIndex = RoomsList.Count - 1;
+				if (RoomsList.Count == 1) { NewIndex = 0; }
+				RoomsList[NewIndex].roomNumber = NewIndex + 1;
 
-            //escaperoom room2 = new escaperoom("sci-fi");
-            //room2.roomnumber = 2;
-            //room2.roomprice = 23.95;
-            //room2.roomsize = "3-5 mensen";
-            //room2.agelimit = 12;
-            //room2.roomavailability = false;
-            //room2.roomtheme = "deze escape room bestaat uit een sci-fi thema waarbij veel aandacht wordt besteed aan sterren puzzels.";
-            //room2.roomduration = "1 uur";
-            //room2.roomreview = "";
-            //room2.roomname = "sci-fi";
+				Console.WriteLine("Enter the minimum age for the escape room:");
+				RoomsList[NewIndex].ageMinimum = Convert.ToInt32(Console.ReadLine());
 
-            //escaperoom room3 = new escaperoom("fantasy");
-            //room3.roomnumber = 3;
-            //room3.roomprice = 23.95;
-            //room3.roomsize = "3-5 mensen";
-            //room3.agelimit = 12;
-            //room3.roomavailability = false;
-            //room3.roomtheme = "deze escape room bestaat uit een fantasy thema waarbij veel aandacht wordt besteed aan oude klassieke puzzels.";
-            //room3.roomduration = "1.5 uur";
-            //room3.roomreview = "";
-            //room3.roomname = "fantasy";
+				while (!(RoomsList[NewIndex].roomSize > 6 & RoomsList[NewIndex].roomSize < 2))
+					Console.WriteLine("Enter the amount of players for the escape room:");
+				if (RoomsList[NewIndex].roomSize < 2 & RoomsList[NewIndex].roomSize > 6) { RoomsList[NewIndex].roomSize = Convert.ToInt32(Console.ReadLine()); }
+				else { Console.WriteLine("Please enter a valid number inbetween 2-6"); }
 
-            //escaperoom room4 = new escaperoom("detective");
-            //room4.roomnumber = 4;
-            //room4.roomprice = 19.95;
-            //room4.roomsize = "5 mensen";
-            //room4.agelimit = 14;
-            //room4.roomavailability = false;
-            //room4.roomtheme = "deze escape room bestaat uit een detective thema waarbij veel oude moordzaken moeten worden opgelost.";
-            //room4.roomduration = "30 minuten";
-            //room4.roomreview = "";
-            //room4.roomname = "detective";
+				Console.WriteLine("Enter the price for the escape room:");
+				RoomsList[NewIndex].roomPrice = Convert.ToDouble(Console.ReadLine());
 
-            //escaperoom room5 = new escaperoom("aquatic");
-            //room5.roomnumber = 5;
-            //room5.roomprice = 35.95;
-            //room5.roomsize = "4-6 mensen";
-            //room5.agelimit = 12;
-            //room5.roomavailability = false;
-            //room5.roomtheme = "deze escape room bestaat uit een aquatic thema waarbij je veel kennis nodig hebt over de ocean en dergelijke.";
-            //room5.roomduration = "1.5 uur";
-            //room5.roomreview = "";
-            //room5.roomname = "aquatic";
+				Console.WriteLine("Enter the theme for the escape room:");
+				RoomsList[NewIndex].roomTheme = Console.ReadLine();
 
-            //Console.Clear();
-            //Console.WriteLine("Maak een keuze voor een Escape Room:");
-            //Console.WriteLine("====================================");
-            //Console.WriteLine(Room1.roomNumber + ") " + Room1.roomName + " " + Room1.ageLimit + "+\n");
-            //Console.WriteLine(Room2.roomNumber + ") " + Room2.roomName + " " + Room2.ageLimit + "+\n");
-            //Console.WriteLine(Room3.roomNumber + ") " + Room3.roomName + " " + Room3.ageLimit + "+\n");
-            //Console.WriteLine(Room4.roomNumber + ") " + Room4.roomName + " " + Room4.ageLimit + "+\n");
-            //Console.WriteLine(Room5.roomNumber + ") " + Room5.roomName + " " + Room5.ageLimit + "+\n");
-            //Console.WriteLine("====================================\n");
-            //Console.WriteLine("Escaperoom Admin opties:\n");
-            //Console.WriteLine("6) Add new Escaperoom\n");
-            //Console.WriteLine("7) Edit Escaperooms\n");
-            //Console.WriteLine("8) Remove Escaperoom\n");
+				Console.WriteLine("Enter the duration for the escape room:");
+				RoomsList[NewIndex].roomDuration = Console.ReadLine();
 
-            //switch(Console.ReadLine())
-            //{
-            //    case "1":
-            //        Console.Clear();
-            //        Console.WriteLine(Room1.roomTheme);
-            //        break;
-            //    case "2":
-            //        Console.Clear();
-            //        Console.WriteLine(Room2.roomTheme);
-            //        break;
-            //    case "3":
-            //        Console.Clear();
-            //        Console.WriteLine(Room3.roomTheme);
-            //        break;
-            //    case "4":
-            //        Console.Clear();
-            //        Console.WriteLine(Room4.roomTheme);
-            //        break;
-            //    case "5":
-            //        Console.Clear();
-            //        Console.WriteLine(Room5.roomTheme);
-            //        break;
-            //    case "6": //addEscapeRoom()
-            //        Console.Clear();
-            //        Console.WriteLine("What is the room name?\n");
-            //        //string x = Console.ReadLine("Samisannoying");
-            //        //EscapeRoom  = new EscapeRoom(userInput);
-            //        break;
-            //    case "7": //editEscapeRoom()
-            //          Console.Clear();
-            //        Console.WriteLine("test 7");
-            //        break;
-            //    case "8": //removeEscapeRoom()
-            //        Console.Clear();
-            //        Console.WriteLine("Which room do you want to delete?");
-            //        Console.WriteLine("====================================");
-            //        Console.WriteLine(Room1.roomNumber + ") " + Room1.roomName + " " + Room1.ageLimit + "+\n");
-            //        Console.WriteLine(Room2.roomNumber + ") " + Room2.roomName + " " + Room2.ageLimit + "+\n");
-            //        Console.WriteLine(Room3.roomNumber + ") " + Room3.roomName + " " + Room3.ageLimit + "+\n");
-            //        Console.WriteLine(Room4.roomNumber + ") " + Room4.roomName + " " + Room4.ageLimit + "+\n");
-            //        Console.WriteLine(Room5.roomNumber + ") " + Room5.roomName + " " + Room5.ageLimit + "+\n");
-            //        Console.WriteLine("====================================\n");
-            //        Console.ReadLine();
-            //        break;
-            //}
-            //Console.WriteLine("Terug naar het hoofdmenu?: (Ja of Nee)\n");
+				Console.WriteLine("Enter the name for the escape room:");
+				RoomsList[NewIndex].roomName = Console.ReadLine();
 
-            //switch (Console.ReadLine())
-            //{
-            //    case "Ja":
-            //        Main();
-            //        break;
-            //    case "ja":
-            //        Main();
-            //        break;
-            //    case "Nee":
-            //        break;
-            //    case "nee":
-            //        break;
-            
-           }
-        }
-    }
+				Console.WriteLine("Room Complete!");
+				Console.Clear();
+				Main();
+			}
 
+			else
+			{
+				Console.WriteLine("There are already 5 EscapeRooms existing!");
+			}
+		}
+
+		static void EditFunction(List<EscapeRoom> RoomsList)
+		{
+
+		}
+
+		static void DeleteFunction(List<EscapeRoom> RoomsList)
+		{
+
+		}
+	}
+
+}
