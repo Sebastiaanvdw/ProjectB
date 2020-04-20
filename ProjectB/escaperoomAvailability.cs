18ac3ab232e88e420dfc407b09d3e5c17efec4fe
Adam <adamauoladali@gmail.com>
2020-04-20 15:22:46 +0200

    Update escaperoomAvailability.cs

--- ProjectB/escaperoomAvailability.cs
+++ ProjectB/escaperoomAvailability.cs
@@ -15,53 +15,53 @@ namespace ProjectB
             string EscapeRoom5Availability = "Available";
 
 
-            Console.WriteLine("Maak een keuze om de beschikbaarheid te checken van een Escape Room:\n");
-            Console.WriteLine("1) Kamer 1\n");
-            Console.WriteLine("2) Kamer 2\n");
-            Console.WriteLine("3) Kamer 3\n");
-            Console.WriteLine("4) Kamer 4\n");
-            Console.WriteLine("5) Kamer 5\n");
+            Console.WriteLine("Select a room to check it's availability.\n");
+            Console.WriteLine("1) Room 1\n");
+            Console.WriteLine("2) Room 2\n");
+            Console.WriteLine("3) Room 3\n");
+            Console.WriteLine("4) Room 4\n");
+            Console.WriteLine("5) Room 5\n");
 
             switch (Console.ReadLine())
             {
                 case "1":
                     Console.Clear();
-                    Console.WriteLine("Kamer 1 is: ");
+                    Console.WriteLine("Room 1 is: ");
                     Console.WriteLine(EscapeRoom1Availability);
                     break;
                 case "2":
                     Console.Clear();
-                    Console.WriteLine("Kamer 2 is: ");
+                    Console.WriteLine("Room 2 is: ");
                     Console.WriteLine(EscapeRoom2Availability);
                     break;
                 case "3":
                     Console.Clear();
-                    Console.WriteLine("Kamer 3 is: ");
+                    Console.WriteLine("Room 3 is: ");
                     Console.WriteLine(EscapeRoom3Availability);
                     break;
                 case "4":
                     Console.Clear();
-                    Console.WriteLine("Kamer 4 is: ");
+                    Console.WriteLine("Room 4 is: ");
                     Console.WriteLine(EscapeRoom4Availability);
                     break;
                 case "5":
                     Console.Clear();
-                    Console.WriteLine("Kamer 5 is: ");
+                    Console.WriteLine("Room 5 is: ");
                     Console.WriteLine(EscapeRoom5Availability);
                     break;
             }
 
 
-            Console.WriteLine("Wil je de beschikbaarheid van de kamer aanpassen?\n");
+            Console.WriteLine("Would you like to change the room's availability?\n");
 
             switch (Console.ReadLine())
 
             {
-                case "ja":
-                    Console.WriteLine("Kamer is nu niet langer beschikbaar.");
+                case "yes":
+                    Console.WriteLine("Room is now no longer available.");
                     break;
-                case "nee":
-                    Console.WriteLine("Is goed.");
+                case "no":
+                    Console.WriteLine("N/A");
                     break;
             }
         }
